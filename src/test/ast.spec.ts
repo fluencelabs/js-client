44e1cc1d8035b47264f4ed3bb75cc4e101b132be
Pavel Murygin <pavel.murygin@gmail.com>
2020-12-23 15:12:07 +0300

    fix ast parsing test

--- src/test/ast.spec.ts
+++ src/test/ast.spec.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import 'mocha';
 import Fluence from '../fluence';
 
@@ -7,6 +8,15 @@ describe('== AST parsing suite', () => {
             (call node ("service" "function") [1 2 3 arg] output)
         `);
 
-        console.log(ast);
+        ast = JSON.parse(ast);
+
+        expect(ast).to.deep.equal({
+            Call: {
+                peer_part: { PeerPk: { Variable: 'node' } },
+                function_part: { ServiceIdWithFuncName: [{ Literal: 'service' }, { Literal: 'function' }] },
+                args: [{ Variable: '1' }, { Variable: '2' }, { Variable: '3' }, { Variable: 'arg' }],
+                output: { Scalar: 'output' },
+            },
+        });
     });
 });
