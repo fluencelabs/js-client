c84a3ba0f3ba718591d7c86a52907021dcdd9a36
Pavel Murygin <pavel.murygin@gmail.com>
2021-01-08 18:16:46 +0300

    fix entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const production = (process.env.NODE_ENV === 'production');
 
 const config = {
-    entry: {
-        app: ['./src/FluenceClient.ts', './src/internal/peerIdUtils.ts', './src/index.ts']
-    },
+    entry: './src/index.ts',
     module: {
         rules: [
             {
