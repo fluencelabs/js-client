0eeba71e20b436e8ef9ee541f5e0d84135eaea4d
Pavel Murygin <pavel.murygin@gmail.com>
2021-01-08 15:59:54 +0300

    trying to fix exports

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@
  * limitations under the License.
  */
 
-export * from './internal/peerIdUtils';
-export * from './FluenceClient';
+export { seedToPeerId, peerIdToSeed, generatePeerId } from './internal/peerIdUtils';
+export { FluenceClient } from './FluenceClient';
