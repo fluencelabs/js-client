5c588330747ad5c21c8614cc814479193a1d3e8d
Pavel Murygin <pavel.murygin@gmail.com>
2021-01-25 22:55:13 +0300

    Add some JSDoc

--- src/api.ts
+++ src/api.ts
@@ -10,6 +10,12 @@ type Node = {
     multiaddr: string;
 };
 
+/**
+ * Creates a Fluence client. If the `connectTo` is specified connects the client to the network
+ * @param { string | Multiaddr | Node } [connectTo] - Node in Fluence network to connect to. If not specified client will not be connected to the n
+ * @param { PeerId | string } [peerIdOrSeed] - The Peer Id of the created client. Specified either as PeerId structure or as seed string. Will be generated randomly if not specified
+ * @returns { Promise<FluenceClient> } Promise which will be resolved with the created FluenceClient
+ */
 export const createClient = async (
     connectTo?: string | Multiaddr | Node,
     peerIdOrSeed?: PeerId | string,
@@ -42,10 +48,22 @@ export const createClient = async (
     return client;
 };
 
+/**
+ * Send a particle to Fluence Network using the specified Fluence Client.
+ * @param { FluenceClient } client - The Fluence Client instance.
+ * @param { Particle } particle  - The particle to send.
+ */
 export const sendParticle = async (client: FluenceClient, particle: Particle): Promise<string> => {
     return await client.sendScript(particle.script, particle.data, particle.ttl);
 };
 
+/**
+ * Registers a function which can be called on the client from Aquamarine. The registration is per client basis.
+ * @param { FluenceClient } client - The Fluence Client instance.
+ * @param { string } serviceId - The identifier of service which would be used to make calls from Aquamarine
+ * @param { string } fnName - The identifier of function which would be used to make calls from Aquamarine
+ * @param { (args: any[], tetraplets: SecurityTetraplet[][]) => object } handler - The handler which would be called by Aquamarine infrastructure. The result is any object passed back to Aquamarine
+ */
 export const registerServiceFunction = (
     client: FluenceClient,
     serviceId: string,
@@ -56,6 +74,12 @@ export const registerServiceFunction = (
 };
 
 // prettier-ignore
+/**
+ * Removes registers for the function previously registered with {@link registerServiceFunction}
+ * @param { FluenceClient } client - The Fluence Client instance.
+ * @param { string } serviceId - The identifier of service used in {@link registerServiceFunction} call
+ * @param { string } fnName - The identifier of function used in {@link registerServiceFunction} call
+ */
 export const unregisterServiceFunction = (
     client: FluenceClient,
     serviceId: string,
@@ -64,6 +88,15 @@ export const unregisterServiceFunction = (
     client.unregisterCallback(serviceId, fnName);
 };
 
+/**
+ * Registers an event-like handler for all calls to the specific service\function pair from from Aquamarine. The registration is per client basis. Return a function which when called removes the subscription.
+ * Same as registerServiceFunction which immediately returns empty object.
+ * @param { FluenceClient } client - The Fluence Client instance.
+ * @param { string } serviceId - The identifier of service calls to which from Aquamarine are transformed into events.
+ * @param { string } fnName - The identifier of function calls to which from Aquamarine are transformed into events.
+ * @param { (args: any[], tetraplets: SecurityTetraplet[][]) => object } handler - The handler which would be called by Aquamarine infrastructure
+ * @returns { Function } - A function which when called removes the subscription.
+ */
 export const subscribeToEvent = (
     client: FluenceClient,
     serviceId: string,
@@ -84,6 +117,14 @@ export const subscribeToEvent = (
     };
 };
 
+/**
+ * Send a particle with a fetch-like semantics. In order to for this to work you have to you have to make a call to the same callbackServiceId\callbackFnName pair from Air script as specified by the parameters. The arguments of the call are returned as the resolve value of promise
+ * @param { FluenceClient } client - The Fluence Client instance.
+ * @param { Particle } particle  - The particle to send.
+ * @param { string } callbackFnName - The identifier of function which should be used in Air script to pass the data to fetch "promise"
+ * @param { [string]='_callback' } callbackServiceId - The service identifier which should be used in Air script to pass the data to fetch "promise"
+ * @returns { Promise<T> } - A promise which would be resolved with the data returned from Aquamarine
+ */
 export const sendParticleAsFetch = async <T>(
     client: FluenceClient,
     particle: Particle,
