d8313d6044956d7a845aaa204a7e5d3ca2db50b9
Pavel Murygin <pavel.murygin@gmail.com>
2021-03-01 11:19:58 +0300

    Made particle signature test deterministic

--- src/__test__/unit/RequestFlow.spec.ts
+++ src/__test__/unit/RequestFlow.spec.ts
@@ -1,18 +1,18 @@
 import PeerId from 'peer-id';
-import { verifyParticle } from '../../internal/particle';
+import { genUUID } from '../../internal/particle';
 import { seedToPeerId } from '../../internal/peerIdUtils';
 import { RequestFlow } from '../../internal/RequestFlow';
 
-const seed = '4vzv3mg6cnjpEK24TXXLA3Ye7QrvKWPKqfbDvAKAyLK6';
-const mockDate = new Date(Date.UTC(2021, 2, 14)).valueOf();
-
 describe('Request flow tests', () => {
-    Date.now = jest.fn(() => mockDate);
-
     it('particle initiation should work', async () => {
         // arrange
         jest.useFakeTimers();
-        const request = RequestFlow.createLocal('(null)', 10000);
+        const seed = '4vzv3mg6cnjpEK24TXXLA3Ye7QrvKWPKqfbDvAKAyLK6';
+        const mockDate = new Date(Date.UTC(2021, 2, 14)).valueOf();
+        Date.now = jest.fn(() => mockDate);
+
+        const request = new RequestFlow(false, '9986a562-7a66-11eb-9439-0242ac130002', '(null)');
+        request.ttl = 10000;
         const peerId = await seedToPeerId(seed);
 
         // act
@@ -21,12 +21,13 @@ describe('Request flow tests', () => {
         // assert
         const particle = request.getParticle();
         expect(particle).toMatchObject({
+            id: '9986a562-7a66-11eb-9439-0242ac130002',
             init_peer_id: peerId.toB58String(),
             script: '(null)',
+            signature: '441A5VjNQN3Bq1KhuQZjsxpYt8xhkC453H3v5QCiQLJkxf48dnJpzk86MxfhT13ZUVs27GC8Q7yLLVTR2NFBE56g',
             timestamp: mockDate,
             ttl: 10000,
         });
-        expect(await verifyParticle(peerId, particle)).toBeTruthy();
         expect(setTimeout).toHaveBeenCalledTimes(1);
     });
 });
