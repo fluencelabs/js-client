365a8705abfa614ec6da99c9f9a7f99e1b10ea60
folex <0xdxdy@gmail.com>
2021-04-12 15:18:10 +0300

    rename spec file

--- src/__test__/unit/WsTransport.spec.ts
+++ src/__test__/unit/WsTransport.spec.ts
@@ -5,20 +5,21 @@ import {createClient} from "../../FluenceClient";
 import {FluenceConnection} from "../../internal/FluenceConnection";
 import Peer from "libp2p";
 import Multiaddr = require("multiaddr");
+import * as PeerId from "peer-id";
 
 describe('Ws Transport', () => {
     it('Should work with ws schema', async () => {
         // arrange
+        let multiaddr = new Multiaddr("/ip4/127.0.0.1/tcp1234/ws/12D3KooWMJ78GJrtCxVUpjLEedbPtnLDxkFQJ2wuefEdrxq6zwSs");
         const connection = new FluenceConnection(
-            null,
-            null,
-            null,
-            null
+            multiaddr,
+            await PeerId.create(),
+            await PeerId.create(),
+            _ => {},
         );
         let node = (connection as any).node as Peer;
 
         // act
-        let multiaddr = new Multiaddr("/ip4/127.0.0.1/tcp1234/ws/12D3KooWMJ78GJrtCxVUpjLEedbPtnLDxkFQJ2wuefEdrxq6zwSs");
         let transport = node.transportManager.transportForMultiaddr(multiaddr);
 
         // assert
