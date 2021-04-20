7a1557e191d8f073c181f8a0a0c9b9596c378291
Pavel Murygin <pavel.murygin@gmail.com>
2021-04-20 13:11:19 +0300

    Fix an issue when 0 was transformed into {}

--- src/__test__/unit/WsTransport.spec.ts
+++ src/__test__/unit/WsTransport.spec.ts
@@ -1,20 +1,16 @@
-import {FluenceConnection} from "../../internal/FluenceConnection";
-import Peer from "libp2p";
-import Multiaddr = require("multiaddr");
-import {generatePeerId} from "../../internal/peerIdUtils";
+import { FluenceConnection } from '../../internal/FluenceConnection';
+import Peer from 'libp2p';
+import Multiaddr = require('multiaddr');
+import { generatePeerId } from '../../internal/peerIdUtils';
 
 describe('Ws Transport', () => {
-    // TODO: fix this test
-    test.skip('Should work with ws schema', async () => {
+    test('Should work with ws schema', async () => {
         // arrange
-        let multiaddr = new Multiaddr("/ip4/127.0.0.1/tcp/1234/ws/p2p/12D3KooWMJ78GJrtCxVUpjLEedbPtnLDxkFQJ2wuefEdrxq6zwSs");
-        let peerId = await generatePeerId();
-        const connection = new FluenceConnection(
-            multiaddr,
-            peerId,
-            peerId,
-            _ => {},
+        let multiaddr = new Multiaddr(
+            '/ip4/127.0.0.1/tcp/1234/ws/p2p/12D3KooWMJ78GJrtCxVUpjLEedbPtnLDxkFQJ2wuefEdrxq6zwSs',
         );
+        let peerId = await generatePeerId();
+        const connection = new FluenceConnection(multiaddr, peerId, peerId, (_) => {});
         await (connection as any).createPeer();
         let node = (connection as any).node as Peer;
 
@@ -22,6 +18,6 @@ describe('Ws Transport', () => {
         let transport = node.transportManager.transportForMultiaddr(multiaddr);
 
         // assert
-        expect(transport).not.toBeDefined();
+        expect(transport).toBeDefined();
     });
 });
