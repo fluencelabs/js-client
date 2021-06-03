d62fa69a26f49dee8a5a11a30bb93398048d4feb
Pavel Murygin <pavel.murygin@gmail.com>
2021-06-03 14:58:04 +0300

    remove unused error class

--- src/internal/FluenceConnection.ts
+++ src/internal/FluenceConnection.ts
@@ -54,15 +54,6 @@ export interface FluenceConnectionOptions {
     dialTimeout?: number;
 }
 
-export class VersionIncompatibleError extends Error {
-    __proto__: Error;
-    constructor() {
-        const trueProto = new.target.prototype;
-        super('Current version of JS SDK is incompatible with the connected Fluence node. Please update JS SDK');
-        this.__proto__ = trueProto;
-    }
-}
-
 export class FluenceConnection {
     private readonly selfPeerId: PeerId;
     private node: Peer;
