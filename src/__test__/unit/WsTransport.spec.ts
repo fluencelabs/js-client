9ab568eb87e380d24ac51631629a84772f9e62ab
Pavel <Pavel.Murygin@gmail.com>
2021-08-09 23:09:32 +0300

    fix one more test

--- src/__test__/unit/WsTransport.spec.ts
+++ src/__test__/unit/WsTransport.spec.ts
@@ -1,7 +1,7 @@
 import { FluenceConnection } from '../../internal/FluenceConnection';
 import Peer from 'libp2p';
 import Multiaddr = require('multiaddr');
-import { generatePeerId } from '../../internal/peerIdUtils';
+import { randomPeerId } from '../../internal/peerIdUtils';
 
 describe('Ws Transport', () => {
     // TODO:: fix test
@@ -10,7 +10,7 @@ describe('Ws Transport', () => {
         let multiaddr = new Multiaddr(
             '/ip4/127.0.0.1/tcp/1234/ws/p2p/12D3KooWMJ78GJrtCxVUpjLEedbPtnLDxkFQJ2wuefEdrxq6zwSs',
         );
-        let peerId = await generatePeerId();
+        let peerId = await randomPeerId();
         const connection = new FluenceConnection(multiaddr, peerId, peerId, (_) => {});
         await (connection as any).createPeer();
         let node = (connection as any).node as Peer;
