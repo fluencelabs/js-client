a78773bd432dcce44f60fa2bb47b0103ded6251c
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-03-11 14:14:12 +0300

    more test cases

--- src/__test__/unit/compiler/v3.spec.ts
+++ src/__test__/unit/compiler/v3.spec.ts
@@ -60,22 +60,115 @@ const structs = [
     },
 ];
 
+const labeledProduct2 = {
+    tag: 'labeledProduct',
+    fields: {
+        x: i32,
+        y: i32,
+    },
+};
+
+const nestedLabeledProductType = {
+    tag: 'labeledProduct',
+    fields: {
+        a: labeledProduct2,
+        b: {
+            tag: 'option',
+            type: labeledProduct2,
+        },
+        c: {
+            tag: 'array',
+            type: labeledProduct2,
+        },
+    },
+};
+
+const nestedStructs = [
+    {
+        aqua: {
+            a: {
+                x: 1,
+                y: 2,
+            },
+            b: [
+                {
+                    x: 1,
+                    y: 2,
+                },
+            ],
+            c: [
+                {
+                    x: 1,
+                    y: 2,
+                },
+                {
+                    x: 3,
+                    y: 4,
+                },
+            ],
+        },
+
+        ts: {
+            a: {
+                x: 1,
+                y: 2,
+            },
+            b: {
+                x: 1,
+                y: 2,
+            },
+
+            c: [
+                {
+                    x: 1,
+                    y: 2,
+                },
+                {
+                    x: 3,
+                    y: 4,
+                },
+            ],
+        },
+    },
+    {
+        aqua: {
+            a: {
+                x: 1,
+                y: 2,
+            },
+            b: [],
+            c: [],
+        },
+
+        ts: {
+            a: {
+                x: 1,
+                y: 2,
+            },
+            b: null,
+            c: [],
+        },
+    },
+];
+
 describe('Conversion from aqua to typescript', () => {
     each`
-    aqua                      | ts                 | type               
-    ${1}                      | ${1}               | ${i32}             
-    ${[]}                     | ${null}            | ${opt_i32}         
-    ${[1]}                    | ${1}               | ${opt_i32}         
-    ${[1, 2, 3]}              | ${[1, 2, 3]}       | ${array_i32}       
-    ${[]}                     | ${[]}              | ${array_i32}       
-    ${[[1]]}                  | ${[1]}             | ${array_opt_i32}   
-    ${[[]]}                   | ${[null]}          | ${array_opt_i32}   
-    ${[[1], [2]]}             | ${[1, 2]}          | ${array_opt_i32}   
-    ${[[], [2]]}              | ${[null, 2]}       | ${array_opt_i32}   
-    ${structs[0].aqua}        | ${structs[0].ts}   | ${labeledProduct}  
-    ${structs[1].aqua}        | ${structs[1].ts}   | ${labeledProduct}  
-    ${structs[0].aqua}        | ${structs[0].ts}   | ${struct}          
-    ${structs[1].aqua}        | ${structs[1].ts}   | ${struct}          
+    aqua                            | ts                       | type               
+    ${1}                            | ${1}                     | ${i32}             
+    ${[]}                           | ${null}                  | ${opt_i32}         
+    ${[1]}                          | ${1}                     | ${opt_i32}         
+    ${[1, 2, 3]}                    | ${[1, 2, 3]}             | ${array_i32}       
+    ${[]}                           | ${[]}                    | ${array_i32}       
+    ${[[1]]}                        | ${[1]}                   | ${array_opt_i32}   
+    ${[[]]}                         | ${[null]}                | ${array_opt_i32}   
+    ${[[1], [2]]}                   | ${[1, 2]}                | ${array_opt_i32}   
+    ${[[], [2]]}                    | ${[null, 2]}             | ${array_opt_i32}   
+    ${structs[0].aqua}              | ${structs[0].ts}         | ${labeledProduct}  
+    ${structs[1].aqua}              | ${structs[1].ts}         | ${labeledProduct}  
+    ${structs[0].aqua}              | ${structs[0].ts}         | ${struct}          
+    ${structs[1].aqua}              | ${structs[1].ts}         | ${struct}          
+    ${nestedStructs[0].aqua}        | ${nestedStructs[0].ts}   | ${nestedLabeledProductType}          
+    ${nestedStructs[1].aqua}        | ${nestedStructs[1].ts}   | ${nestedLabeledProductType}          
 `.test(
         //
         'aqua: $aqua. ts: $ts. type: $type',
