84babfc4764ecefada6e65050474f83cdf2fd7e8
Pavel Murygin <Pavel.Murygin@gmail.com>
2022-08-03 17:51:06 +0300

    Expose parse ast method from AVM

---
+++ src/__test__/unit/ast.spec.ts
@@ -0,0 +1,35 @@
+import { Fluence } from '../../index';
+
+describe('Parse ast tests', () => {
+    beforeAll(async () => {
+        await Fluence.start();
+    });
+
+    afterAll(async () => {
+        await Fluence.stop();
+    });
+
+    it('Correct ast should be parsed correctly', async function () {
+        await Fluence.start();
+        const peer = Fluence.getPeer();
+        const air = `(null)`;
+        const res = await peer.internals.parseAst(air);
+
+        expect(res).toStrictEqual({
+            success: true,
+            data: { Null: null },
+        });
+    });
+
+    it('Incorrect ast should result in corresponding error', async function () {
+        await Fluence.start();
+        const peer = Fluence.getPeer();
+        const air = `(null`;
+        const res = await peer.internals.parseAst(air);
+
+        expect(res).toStrictEqual({
+            success: false,
+            data: expect.stringContaining('error'),
+        });
+    });
+});
