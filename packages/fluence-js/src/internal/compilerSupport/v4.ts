75cefc16ad87c8c2893131a5a25513472eeac997
Pavel <Pavel.Murygin@gmail.com>
2022-10-08 23:18:38 +0300

    Add v4 version of compiler support (#190)

---
+++ packages/fluence-js/src/internal/compilerSupport/v4.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { FluencePeer } from '../FluencePeer';
+export { CallParams as CallParams$$ } from '../commonTypes';
+export {
+    ArrayType as ArrayType$$,
+    ArrowType as ArrowType$$,
+    ArrowWithCallbacks as ArrowWithCallbacks$$,
+    ArrowWithoutCallbacks as ArrowWithoutCallbacks$$,
+    BottomType as BottomType$$,
+    FnConfig as FnConfig$$,
+    FunctionCallConstants as FunctionCallConstants$$,
+    FunctionCallDef as FunctionCallDef$$,
+    LabeledProductType as LabeledProductType$$,
+    NilType as NilType$$,
+    NonArrowType as NonArrowType$$,
+    OptionType as OptionType$$,
+    ProductType as ProductType$$,
+    ScalarNames as ScalarNames$$,
+    ScalarType as ScalarType$$,
+    ServiceDef as ServiceDef$$,
+    StructType as StructType$$,
+    TopType as TopType$$,
+    UnlabeledProductType as UnlabeledProductType$$,
+} from './v3impl/interface';
+export { callFunction as callFunction$$ } from './v3impl/callFunction';
+export { registerService as registerService$$ } from './v3impl/registerService';
