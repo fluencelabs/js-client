64bedb8a3588aeacafe87c9d240067130ceaa917
Pavel Murygin <pavel.murygin@gmail.com>
2022-10-17 20:57:18 +0400

    fixed

--- packages/fluence-js/src/__test__/_aqua/srv-tests.ts
+++ packages/fluence-js/src/__test__/_aqua/srv-tests.ts
@@ -13,6 +13,188 @@ import { CallParams, callFunction, registerService } from '../../internal/compil
 
 // Functions
 
+export function happy_path(file_path: string, config?: { ttl?: number }): Promise<string>;
+
+export function happy_path(peer: FluencePeer, file_path: string, config?: { ttl?: number }): Promise<string>;
+
+export function happy_path(...args: any) {
+    let script = `
+                    (xor
+                     (seq
+                      (seq
+                       (seq
+                        (seq
+                         (seq
+                          (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+                          (call %init_peer_id% ("getDataSrv" "file_path") [] file_path)
+                         )
+                         (call %init_peer_id% ("single_module_srv" "read_file") [file_path] wasm)
+                        )
+                        (call %init_peer_id% ("single_module_srv" "create") [wasm] id)
+                       )
+                       (call %init_peer_id% (id "greeting") ["test"] greeting)
+                      )
+                      (xor
+                       (call %init_peer_id% ("callbackSrv" "response") [greeting])
+                       (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+                      )
+                     )
+                     (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+                    )
+    `;
+    return callFunction(
+        args,
+        {
+            functionName: 'happy_path',
+            arrow: {
+                tag: 'arrow',
+                domain: {
+                    tag: 'labeledProduct',
+                    fields: {
+                        file_path: {
+                            tag: 'scalar',
+                            name: 'string',
+                        },
+                    },
+                },
+                codomain: {
+                    tag: 'unlabeledProduct',
+                    items: [
+                        {
+                            tag: 'scalar',
+                            name: 'string',
+                        },
+                    ],
+                },
+            },
+            names: {
+                relay: '-relay-',
+                getDataSrv: 'getDataSrv',
+                callbackSrv: 'callbackSrv',
+                responseSrv: 'callbackSrv',
+                responseFnName: 'response',
+                errorHandlingSrv: 'errorHandlingSrv',
+                errorFnName: 'error',
+            },
+        },
+        script,
+    );
+}
+
+export function removing_non_exiting(config?: { ttl?: number }): Promise<string>;
+
+export function removing_non_exiting(peer: FluencePeer, config?: { ttl?: number }): Promise<string>;
+
+export function removing_non_exiting(...args: any) {
+    let script = `
+                    (xor
+                     (seq
+                      (seq
+                       (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+                       (xor
+                        (call %init_peer_id% ("single_module_srv" "remove") ["random_id"])
+                        (null)
+                       )
+                      )
+                      (xor
+                       (call %init_peer_id% ("callbackSrv" "response") [%last_error%.$.message!])
+                       (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+                      )
+                     )
+                     (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+                    )
+    `;
+    return callFunction(
+        args,
+        {
+            functionName: 'removing_non_exiting',
+            arrow: {
+                tag: 'arrow',
+                domain: {
+                    tag: 'labeledProduct',
+                    fields: {},
+                },
+                codomain: {
+                    tag: 'unlabeledProduct',
+                    items: [
+                        {
+                            tag: 'scalar',
+                            name: 'string',
+                        },
+                    ],
+                },
+            },
+            names: {
+                relay: '-relay-',
+                getDataSrv: 'getDataSrv',
+                callbackSrv: 'callbackSrv',
+                responseSrv: 'callbackSrv',
+                responseFnName: 'response',
+                errorHandlingSrv: 'errorHandlingSrv',
+                errorFnName: 'error',
+            },
+        },
+        script,
+    );
+}
+
+export function file_not_found(config?: { ttl?: number }): Promise<string>;
+
+export function file_not_found(peer: FluencePeer, config?: { ttl?: number }): Promise<string>;
+
+export function file_not_found(...args: any) {
+    let script = `
+                    (xor
+                     (seq
+                      (seq
+                       (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+                       (xor
+                        (call %init_peer_id% ("single_module_srv" "read_file") ["/random/incorrect/file"] wasm)
+                        (null)
+                       )
+                      )
+                      (xor
+                       (call %init_peer_id% ("callbackSrv" "response") [%last_error%.$.message!])
+                       (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+                      )
+                     )
+                     (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+                    )
+    `;
+    return callFunction(
+        args,
+        {
+            functionName: 'file_not_found',
+            arrow: {
+                tag: 'arrow',
+                domain: {
+                    tag: 'labeledProduct',
+                    fields: {},
+                },
+                codomain: {
+                    tag: 'unlabeledProduct',
+                    items: [
+                        {
+                            tag: 'scalar',
+                            name: 'string',
+                        },
+                    ],
+                },
+            },
+            names: {
+                relay: '-relay-',
+                getDataSrv: 'getDataSrv',
+                callbackSrv: 'callbackSrv',
+                responseSrv: 'callbackSrv',
+                responseFnName: 'response',
+                errorHandlingSrv: 'errorHandlingSrv',
+                errorFnName: 'error',
+            },
+        },
+        script,
+    );
+}
+
 export function service_removed(file_path: string, config?: { ttl?: number }): Promise<string>;
 
 export function service_removed(peer: FluencePeer, file_path: string, config?: { ttl?: number }): Promise<string>;
@@ -87,11 +269,11 @@ export function service_removed(...args: any) {
     );
 }
 
-export function happy_path(file_path: string, config?: { ttl?: number }): Promise<string>;
+export function list_services(file_path: string, config?: { ttl?: number }): Promise<string[]>;
 
-export function happy_path(peer: FluencePeer, file_path: string, config?: { ttl?: number }): Promise<string>;
+export function list_services(peer: FluencePeer, file_path: string, config?: { ttl?: number }): Promise<string[]>;
 
-export function happy_path(...args: any) {
+export function list_services(...args: any) {
     let script = `
                     (xor
                      (seq
@@ -99,17 +281,23 @@ export function happy_path(...args: any) {
                        (seq
                         (seq
                          (seq
-                          (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
-                          (call %init_peer_id% ("getDataSrv" "file_path") [] file_path)
+                          (seq
+                           (seq
+                            (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+                            (call %init_peer_id% ("getDataSrv" "file_path") [] file_path)
+                           )
+                           (call %init_peer_id% ("single_module_srv" "read_file") [file_path] wasm)
+                          )
+                          (call %init_peer_id% ("single_module_srv" "create") [wasm])
                          )
-                         (call %init_peer_id% ("single_module_srv" "read_file") [file_path] wasm)
+                         (call %init_peer_id% ("single_module_srv" "create") [wasm])
                         )
-                        (call %init_peer_id% ("single_module_srv" "create") [wasm] id)
+                        (call %init_peer_id% ("single_module_srv" "create") [wasm])
                        )
-                       (call %init_peer_id% (id "greeting") ["test"] greeting)
+                       (call %init_peer_id% ("single_module_srv" "list") [] list)
                       )
                       (xor
-                       (call %init_peer_id% ("callbackSrv" "response") [greeting])
+                       (call %init_peer_id% ("callbackSrv" "response") [list])
                        (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
                       )
                      )
@@ -119,7 +307,7 @@ export function happy_path(...args: any) {
     return callFunction(
         args,
         {
-            functionName: 'happy_path',
+            functionName: 'list_services',
             arrow: {
                 tag: 'arrow',
                 domain: {
@@ -135,8 +323,11 @@ export function happy_path(...args: any) {
                     tag: 'unlabeledProduct',
                     items: [
                         {
-                            tag: 'scalar',
-                            name: 'string',
+                            tag: 'array',
+                            type: {
+                                tag: 'scalar',
+                                name: 'string',
+                            },
                         },
                     ],
                 },
