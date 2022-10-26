eb13d66b651098d2535088350e77bf53d8eb2f2a
Pavel Murygin <pavel.murygin@gmail.com>
2022-10-26 14:42:24 +0400

    Error handling

--- packages/fluence-js/src/internal/builtins/common.ts
+++ packages/fluence-js/src/internal/builtins/common.ts
@@ -18,7 +18,7 @@ import { encode, decode } from 'bs58';
 import { sha256 } from 'multiformats/hashes/sha2';
 import { CallServiceResult } from '@fluencelabs/avm';
 
-import { GenericCallServiceHandler, ResultCodes } from '../commonTypes';
+import { CallServiceData, GenericCallServiceHandler, ResultCodes } from '../commonTypes';
 import { jsonify } from '../utils';
 import Buffer from '../Buffer';
 
@@ -38,6 +38,23 @@ const error = (error: string): CallServiceResult => {
 
 const errorNotImpl = (methodName: string) => {
     return error(`The JS implementation of Peer does not support "${methodName}"`);
+};
+
+const makeJsonImpl = (args: Array<any>) => {
+    const [obj, ...kvs] = args;
+
+    const toMerge: Record<string, any> = {};
+    for (let i = 0; i < kvs.length / 2; i++) {
+        const k = kvs[i * 2];
+        if (!isString(k)) {
+            return error(`Argument ${k} is expected to be string`);
+        }
+        const v = kvs[i * 2 + 1];
+        toMerge[k] = v;
+    }
+
+    const res = { ...obj, ...toMerge };
+    return success(res);
 };
 
 export const builtInServices: Record<string, Record<string, GenericCallServiceHandler>> = {
@@ -470,30 +487,53 @@ export const builtInServices: Record<string, Record<string, GenericCallServiceHa
 
     json: {
         obj: (req) => {
-            const res = intoJson(req.args);
-            return success(res);
+            let err;
+            if ((err = checkForArgumentsCountEven(req, 1))) {
+                return err;
+            }
+
+            return makeJsonImpl([{}, ...req.args]);
         },
 
         put: (req) => {
-            const [obj, k, v] = req.args;
-            obj[k] = v;
-            return success(obj);
+            let err;
+            if ((err = checkForArgumentsCount(req, 3))) {
+                return err;
+            }
+
+            return makeJsonImpl(req.args);
         },
 
         puts: (req) => {
-            const [obj, ...kvs] = req.args;
-            const toMerge = intoJson(kvs);
-            const res = { ...obj, ...toMerge };
-            return success(res);
+            let err;
+            if ((err = checkForArgumentsCountOdd(req, 1))) {
+                return err;
+            }
+
+            if ((err = checkForArgumentsCountMoreThan(req, 3))) {
+                return err;
+            }
+
+            return makeJsonImpl(req.args);
         },
 
         stringify: (req) => {
+            let err;
+            if ((err = checkForArgumentsCount(req, 1))) {
+                return err;
+            }
+
             const [json] = req.args;
             const res = JSON.stringify(json);
             return success(res);
         },
 
         parse: (req) => {
+            let err;
+            if ((err = checkForArgumentsCount(req, 1))) {
+                return err;
+            }
+
             const [raw] = req.args;
             const json = JSON.parse(raw);
             return success(json);
@@ -507,12 +547,24 @@ const checkForArgumentsCount = (req: { args: Array<unknown> }, count: number) =>
     }
 };
 
-const intoJson = (kvs: Array<unknown>) => {
-    const res: Record<string, any> = {};
-    for (let i = 0; i < kvs.length / 2; i++) {
-        const k = kvs[i * 2] as string;
-        const v = kvs[i * 2 + 1];
-        res[k] = v;
+const checkForArgumentsCountMoreThan = (req: { args: Array<unknown> }, count: number) => {
+    if (req.args.length < count) {
+        return error(`Expected more than ${count} argument(s). Got ${req.args.length}`);
     }
-    return res;
+};
+
+const checkForArgumentsCountEven = (req: { args: Array<unknown> }, count: number) => {
+    if (req.args.length % 2 === 1) {
+        return error(`Expected even number of argument(s). Got ${req.args.length}`);
+    }
+};
+
+const checkForArgumentsCountOdd = (req: { args: Array<unknown> }, count: number) => {
+    if (req.args.length % 2 === 0) {
+        return error(`Expected odd number of argument(s). Got ${req.args.length}`);
+    }
+};
+
+export const isString = (unknown: unknown): unknown is string => {
+    return unknown !== null && typeof unknown === 'string';
 };
