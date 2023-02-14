a4d851f2ff426d82df2296840184af5a5fa0d092
Pavel Murygin <pavel.murygin@gmail.com>
2023-02-15 03:59:15 +0400

    PR fixes

--- packages/client/api/src/util.ts
+++ packages/client/api/src/util.ts
@@ -8,13 +8,22 @@ const getPeerFromGlobalThis = (): IFluenceClient | undefined => {
 // TODO: DXJ-271
 const REJECT_MESSAGE = 'You probably forgot to add script tag. Read about it here: ';
 
+// Let's assume that if the library has not been loaded in 5 seconds, then the user has forgotten to add the script tag
+const POLL_PEER_TIMEOUT = 5000;
+
+// The script might be cached so need to try loading it ASAP, thus short interval
+const POLL_PEER_INTERVAL = 100;
+
 /**
  * Wait until the js client script it loaded and return the default peer from globalThis
  */
 export const getDefaultPeer = (): Promise<IFluenceClient> => {
     return new Promise((resolve, reject) => {
-        let interval: NodeJS.Timer | undefined;
-        let hits = 50;
+        // This function is internal
+        // Make it sure that would be zero way for unnecessary types
+        // to break out into the public API
+        let interval: any;
+        let hits = POLL_PEER_TIMEOUT / POLL_PEER_INTERVAL;
         interval = setInterval(() => {
             if (hits === 0) {
                 clearInterval(interval);
@@ -27,6 +36,6 @@ export const getDefaultPeer = (): Promise<IFluenceClient> => {
                 resolve(res);
             }
             hits--;
-        }, 100);
+        }, POLL_PEER_INTERVAL);
     });
 };
