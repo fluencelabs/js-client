ac8d1b7ede0eff384d9e4148b602aaf82f6846ed
DieMyst <dmitry.shakhtarin@fluence.ai>
2023-02-22 14:28:47 +0400

    delete comments

--- packages/core/js-peer/jest.config.cjs
+++ packages/core/js-peer/jest.config.cjs
@@ -1,13 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     extensionsToTreatAsEsm: ['.ts'],
-    "preset": "ts-jest/presets/default-esm", // or other ESM presets,
+    "preset": "ts-jest/presets/default-esm",
     "moduleNameMapper": {
         "^(\\.{1,2}/.*)\\.js$": "$1"
     },
     "transform": {
-        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
         "^.+\\.tsx?$": [
             "ts-jest",
             {
