18cd2942244e2b60c8c03e935608cbdcd4224f62
Akim <59872966+akim-bow@users.noreply.github.com>
2023-09-29 18:51:11 +0700

    Update packages/core/js-client/src/connection/RelayConnection.ts
    Co-authored-by: shamsartem <shamsartem@gmail.com>

--- packages/core/js-client/src/connection/RelayConnection.ts
+++ packages/core/js-client/src/connection/RelayConnection.ts
@@ -193,7 +193,9 @@ export class RelayConnection implements IConnection {
                 log.trace('particle signature is incorrect. rejecting particle with id: %s', particle.id);
             }
         } catch (e) {
-            log.error('error on handling an incoming message: %O. particle id: %s', e, particle?.id ?? '0');
+            const particleId = particle?.id
+            const particleIdMessage = typeof particleId === 'string' ? `. particle id: ${particleId}` : '';
+            log.error(`error on handling an incoming message: %O%s`, e, particleIdMessage);
         }
     }
 
