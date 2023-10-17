73ba1dfa292b205e4a6e94da34a5598c6e03e644
Akim Mamedov <akim99999999@gmail.com>
2023-10-17 23:27:08 +0700

    Fix lint

--- packages/core/js-client/src/index.ts
+++ packages/core/js-client/src/index.ts
@@ -29,9 +29,10 @@ import { BlobWorker, Worker } from "threads/master";
 import { ClientPeer, makeClientPeerConfig } from "./clientPeer/ClientPeer.js";
 import { callAquaFunction } from "./compilerSupport/callFunction.js";
 import { registerService } from "./compilerSupport/registerService.js";
-import { fetchResource } from "#fetcher";
 import { MarineBackgroundRunner } from "./marine/worker/index.js";
 import { doRegisterNodeUtils } from "./services/NodeUtils.js";
+
+import { fetchResource } from "#fetcher";
 
 const isNode =
   // process.release is undefined in browser env
