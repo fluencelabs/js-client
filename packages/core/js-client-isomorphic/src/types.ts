94f0e439a2102f6648892151c077faf7e6d39d79
Akim Mamedov <akim99999999@gmail.com>
2023-10-25 18:03:12 +0700

    Fix newline

--- packages/core/js-client-isomorphic/src/types.ts
+++ packages/core/js-client-isomorphic/src/types.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Worker } from "threads/master";
 
 export type VersionedPackage = { name: string; version: string };
