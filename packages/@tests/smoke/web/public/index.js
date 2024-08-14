578e428e43eed633fe2ce87e70fafdd5b4bbf08d
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-08-14 10:59:15 +0200

    fix

--- packages/@tests/smoke/web/public/index.js
+++ packages/@tests/smoke/web/public/index.js
@@ -2,8 +2,8 @@ import { Fluence, callAquaFunction } from "./js-client.min.js";
 
 const relay = {
   multiaddr:
-    "/ip4/127.0.0.1/tcp/9991/ws/p2p/12D3KooWHQs4WtPDwsgJdQpbXUakdZk8THyBHSTbdnJEyPPFvd3H",
-  peerId: "12D3KooWHQs4WtPDwsgJdQpbXUakdZk8THyBHSTbdnJEyPPFvd3H",
+    "/ip4/127.0.0.1/tcp/9991/ws/p2p/12D3KooWBbMuqJJZT7FTFN4fWg3k3ipUKx6KEy7pDy8mdorK5g5o",
+  peerId: "12D3KooWBbMuqJJZT7FTFN4fWg3k3ipUKx6KEy7pDy8mdorK5g5o",
 };
 
 const getRelayTime = () => {
--- packages/core/js-client/src/clientPeer/__test__/connection.ts
+++ packages/core/js-client/src/clientPeer/__test__/connection.ts
@@ -17,8 +17,8 @@
 export const nodes = [
   {
     multiaddr:
-      "/ip4/127.0.0.1/tcp/9991/ws/p2p/12D3KooWHQs4WtPDwsgJdQpbXUakdZk8THyBHSTbdnJEyPPFvd3H",
-    peerId: "12D3KooWHQs4WtPDwsgJdQpbXUakdZk8THyBHSTbdnJEyPPFvd3H",
+      "/ip4/127.0.0.1/tcp/9991/ws/p2p/12D3KooWBbMuqJJZT7FTFN4fWg3k3ipUKx6KEy7pDy8mdorK5g5o",
+    peerId: "12D3KooWBbMuqJJZT7FTFN4fWg3k3ipUKx6KEy7pDy8mdorK5g5o",
   },
 ] as const;
 
